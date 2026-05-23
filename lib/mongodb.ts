99b781d49870af7ddfa03e1d96db1869df1ee505
Ashraf ali <ashraf.ali@sabdullahome.com>
2026-05-23 10:54:00 +0500

    update

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -4,7 +4,7 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Please add your MongoDB URI to .env.local')
 }
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI || 'test'
 const options = {}
 
 let client: MongoClient
